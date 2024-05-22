4fb1a4ee6a8702ca05811ba5d499202deeb13163
Max Kagamine <max@kgmn.jp>
2024-05-23 00:58:22 +0900

    Set locale & charset

--- SqliteArchive.Server/Controllers/SqlarController.cs
+++ SqliteArchive.Server/Controllers/SqlarController.cs
@@ -49,6 +49,11 @@ public class SqlarController : Controller
                 contentType = "application/octet-stream";
             }
 
+            if (!string.IsNullOrEmpty(options.Charset))
+            {
+                contentType += "; charset=" + options.Charset;
+            }
+
             return File(stream, contentType);
         }
 
--- SqliteArchive.Server/ServerOptions.cs
+++ SqliteArchive.Server/ServerOptions.cs
@@ -9,18 +9,25 @@ public record ServerOptions
         Environment variables:
 
         TZ                     Timezone for displaying date modified (default: UTC)
-                               See [3;34m]8;;https://w.wiki/4Jx\List of tz database time zones]8;;\[m
+                               See [3m]8;;https://w.wiki/4Jx\List of tz database time zones]8;;\[m
+
+        LANG                   Locale used for formatting (default: en_US)
 
         SizeFormat             Bytes = Display file sizes in bytes without formatting
                                Binary = Use binary units (KiB, MiB, GiB, TiB) (default)
                                SI = Use SI units (KB, MB, GB, TB)
 
         SortDirectoriesFirst   Group directories before files (default: true)
+
+        Charset                Sets the charset in the Content-Type header of file
+                               streams. Empty string to disable. (default: utf-8)
         """;
 
     public required SizeFormat SizeFormat { get; init; }
 
     public required bool SortDirectoriesFirst { get; init; }
+
+    public required string Charset { get; init; }
 }
 
 public enum SizeFormat
