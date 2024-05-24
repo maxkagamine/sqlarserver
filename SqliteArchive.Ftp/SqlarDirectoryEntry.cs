e95ff877f04f3c539e7b8e82a202d5047a8d4c49
Max Kagamine <max@kgmn.jp>
2024-05-24 19:06:29 +0900

    Set up the FTP file system provider

---
+++ SqliteArchive.Ftp/SqlarDirectoryEntry.cs
@@ -0,0 +1,14 @@
+﻿using FubarDev.FtpServer.FileSystem;
+using SqliteArchive.Nodes;
+
+namespace SqliteArchive.Ftp;
+
+internal class SqlarDirectoryEntry : SqlarFileSystemEntry, IUnixDirectoryEntry
+{
+    public SqlarDirectoryEntry(DirectoryNode node) : base(node)
+    { }
+
+    public bool IsRoot => Node.Path.IsRoot;
+
+    public bool IsDeletable => false;
+}
---
+++ SqliteArchive.Ftp/SqlarFileEntry.cs
@@ -0,0 +1,12 @@
+﻿using FubarDev.FtpServer.FileSystem;
+using SqliteArchive.Nodes;
+
+namespace SqliteArchive.Ftp;
+
+internal class SqlarFileEntry : SqlarFileSystemEntry, IUnixFileEntry
+{
+    public SqlarFileEntry(Node node) : base(node)
+    { }
+
+    public long Size => Node.Size;
+}
